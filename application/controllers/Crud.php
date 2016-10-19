32f56e3444b41db3a6276c5facf3549705c9c513
Samit <samitkoyom@gmail.com>
2016-10-19 22:08:33 +0700

    first commit

---
+++ application/controllers/Crud.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+* Crud Class Controller
+*/
+class Crud extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		echo "<div style='font-family:Helvetica; font-size:36px; text-align:center; margin-top:50vh'>CIDBProject Work!!!</div>";
+	}
+}
---
+++ readme.rst
@@ -0,0 +1,13 @@
+###################
+CI DB Project
+###################
+
+This project for CRUD with CodeIgniter 3.1.0 in class room.
+
+*******************
+Credit
+*******************
+AJ.Samit Koyom
+IT Genius Engineering
+02-933-0427
+
